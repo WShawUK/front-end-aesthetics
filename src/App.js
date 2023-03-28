7dfc90c5ed76e2b1277d8a4daa5f561f44e8f625
WShawUK <120861331+WShawUK@users.noreply.github.com>
2023-03-28 21:42:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -90,8 +90,11 @@ function ExpandableDiv() {
   const thisElement = useRef(null)
 
   useEffect(() => {
-    console.log(thisElement.current.firstElementChild.clientHeight)
-    thisElement.current.style.maxHeight = thisElement.current.firstElementChild.clientHeight + 'px'
+    thisElement.current.firstElementChild.firstElementChild.addEventListener('load', (e) => {
+      console.log(thisElement.current.firstElementChild.clientHeight)
+      thisElement.current.style.maxHeight = thisElement.current.firstElementChild.clientHeight + 'px'
+    })
+    // thisElement.current.style.maxHeight = thisElement.current.firstElementChild.clientHeight + 'px'
 
     window.addEventListener('resize', () => {
       thisElement.current.style.maxHeight = thisElement.current.firstElementChild.clientHeight + 'px'
@@ -503,6 +506,114 @@ function BlobHeader() {
 
 
 
+
+
+
+function AddableStuff() {
+
+  const ThingToAdd = (args) => {
+    return (
+      <div className="thing-to-add thing-to-add-fade-in" style={{zIndex: args.negativeIndex}}>
+        <div>
+          <input type={'text'} defaultValue={args.name}></input>
+        </div>
+        <div>
+          <button onClick={RemoveButton}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512" pointerEvents={'none'}>
+              <path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="black" strokeMiterlimit="10" strokeWidth="32" />
+              <path fill="none" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M336 256H176" />
+            </svg>
+          </button>
+          <div className="pallet-div">
+            <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512">
+              <path d="M430.11 347.9c-6.6-6.1-16.3-7.6-24.6-9-11.5-1.9-15.9-4-22.6-10-14.3-12.7-14.3-31.1 0-43.8l30.3-26.9c46.4-41 46.4-108.2 0-149.2-34.2-30.1-80.1-45-127.8-45-55.7 0-113.9 20.3-158.8 60.1-83.5 73.8-83.5 194.7 0 268.5 41.5 36.7 97.5 55 152.9 55.4h1.7c55.4 0 110-17.9 148.8-52.4 14.4-12.7 11.99-36.6.1-47.7z" fill="none" stroke="black" strokeMiterlimit="10" strokeWidth="32" />
+              <circle cx="144" cy="208" r="32" />
+              <circle cx="152" cy="311" r="32" />
+              <circle cx="224" cy="144" r="32" />
+              <circle cx="256" cy="367" r="48" />
+              <circle cx="328" cy="144" r="32" />
+            </svg>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+            <div onClick={ChangeColorOfThing} className="pallet-option"></div>
+          </div>
+        </div>
+    </div>
+    )
+  }
+
+  const [itemList, setItemList] = useState([<ThingToAdd name={'list item'} key={0} negativeIndex = {-1}/>, <ThingToAdd name={'list item'} key={1} negativeIndex = {-2}/>, <ThingToAdd name={'list item'} key={2} negativeIndex = {-3}/>])
+  const [itemKey, setItemKey] = useState(3)
+  const [descendingZIndex, setDescendingZIndex] = useState(-4)
+
+
+  const ChangeColorOfThing = (e) => {
+    const colorToChangeTo = window.getComputedStyle(e.target).getPropertyValue('background-color')
+    e.target.parentElement.parentElement.parentElement.style.setProperty('--color-of-thing', colorToChangeTo)
+  }
+
+  const RemoveButton = (e) => {
+    console.log(e.target)
+    e.target.parentElement.children[1].style.pointerEvents = 'none'
+    e.target.parentElement.parentElement.style.opacity = 0
+    e.target.parentElement.parentElement.style.height = '0px'
+    e.target.parentElement.parentElement.style.margin = '0px'
+    e.target.parentElement.parentElement.style.translate = '0 -30px'
+    setTimeout(() => {
+      e.target.parentElement.parentElement.remove()
+    }, 500);
+  }
+
+  const [timerID, setTimerID] = useState(null)
+  const AddItemButton = (e) => {
+    if (!timerID){
+      console.log('no timmer')
+      const inputValue = document.getElementById('add-list-item-input').value
+      const nameToPass = (inputValue) ? inputValue : 'list item';
+      // add random colour
+      setItemList([...itemList, <ThingToAdd name={nameToPass} key={itemKey} negativeIndex={descendingZIndex}/>])
+      setItemKey(itemKey + 1)
+      setDescendingZIndex(descendingZIndex - 1)
+      setTimerID(setTimeout(() => {
+        setTimerID(null)
+      }, 250))
+    }
+  }
+
+
+  return (
+    <div id="addable-stuff-div">
+      <div>
+        <input id='add-list-item-input' type={'text'} placeholder='add item name here...'></input>
+        <button onClick={AddItemButton}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512" pointerEvents={'none'}>
+            <path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="black" strokeMiterlimit="10" strokeWidth="32" />
+            <path fill="none" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M256 176v160M336 256H176" />
+          </svg>
+        </button>
+      </div>
+      {itemList.map((thingToAdd) => thingToAdd)}
+    </div>
+  )
+}
+
+
+
+
+
+
 function EntirePage() {
   return (
     <>
@@ -519,6 +630,7 @@ function EntirePage() {
       <ObservedSlidingElements />
       <RadialMenu />
       <FlashingDivWithButtons />
+      <AddableStuff />
       <BlobHeader />
     </>
   )
